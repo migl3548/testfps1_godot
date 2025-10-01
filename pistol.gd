e198174c644937c263ddfdf2c4899c2652c08e44
Ari <migl3548@colorado.edu>
2025-09-30 19:26:24 -0600

    shooting works

---
+++ pistol.gd
@@ -0,0 +1,15 @@
+extends Node3D
+
+@onready var animation: AnimationPlayer = $Animation
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	animation.play("Idle")
+
+func _input(event) -> void:
+	if event is InputEventMouseButton:
+		animation.play("Shoot", -1, 1.5)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
