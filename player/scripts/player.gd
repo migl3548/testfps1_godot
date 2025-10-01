6b4033b86f1b8bb3916687d30f3d269b0f09b236
Ari <migl3548@colorado.edu>
2025-09-30 18:27:26 -0600

    mouse movement (x)

--- player/scripts/player.gd
+++ player/scripts/player.gd
@@ -1,1 +1,32 @@
 extends CharacterBody3D
+
+const SPEED: float = 10.0
+const MOUSE_SENS: float = 0.1
+const JUMP_VELOCITY = 4.5
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _input(event) -> void:
+	if event is InputEventMouseMotion:
+		rotation_degrees.y -= event.relative.x * MOUSE_SENS
+
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	
+	#jump - comment out if breaking shit
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		
+	#get the input direction and handle movement/decel
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+	
+	move_and_slide()
